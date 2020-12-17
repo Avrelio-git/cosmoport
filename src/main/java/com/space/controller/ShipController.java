a9dcc09529a00fe359c3cb8e4c55d944064d9257
Avrelio <xavrelio@gmail.com>
2020-12-17 14:27:49 +0000

    Stage Test

---
+++ src/main/java/com/space/controller/ShipController.java
@@ -0,0 +1,104 @@
+package com.space.controller;
+
+import com.space.model.Ship;
+import com.space.model.ShipType;
+import com.space.service.ShipService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+
+@RestController
+@Service
+public class ShipController {
+
+    @Autowired
+    private ShipService shipService;
+
+
+    @GetMapping("/rest/ships")
+    public List<Ship> getShipsList(@RequestParam(value = "name", required = false) String name,
+                                   @RequestParam(value = "planet", required = false) String planet,
+                                   @RequestParam(value = "shipType", required = false) ShipType shipType,
+                                   @RequestParam(value = "after", required = false) Long after,
+                                   @RequestParam(value = "before", required = false) Long before,
+                                   @RequestParam(value = "isUsed", required = false) Boolean isUsed,
+                                   @RequestParam(value = "minSpeed", required = false) Double minSpeed,
+                                   @RequestParam(value = "maxSpeed", required = false) Double maxSpeed,
+                                   @RequestParam(value = "minCrewSize", required = false) Integer minCrewSize,
+                                   @RequestParam(value = "maxCrewSize", required = false) Integer maxCrewSize,
+                                   @RequestParam(value = "minRating", required = false) Double minRating,
+                                   @RequestParam(value = "maxRating", required = false) Double maxRating,
+                                   @RequestParam(value = "order", required = false, defaultValue = "ID") ShipOrder order,
+                                   @RequestParam(value = "pageNumber", defaultValue = "0") Integer pageNumber,
+                                   @RequestParam(value = "pageSize", defaultValue = "3") Integer pageSize) {
+
+        Pageable pageable = PageRequest.of(pageNumber, pageSize, Sort.by(order.getFieldName()));
+
+        return shipService.getAll(Specification
+                        .where(shipService.filterStringLike(name,"name")
+                        .and(shipService.filterStringLike(planet,"planet")))
+                        .and(shipService.filterEqual(shipType,"shipType"))
+                        .and(shipService.filterBetween(after, before,"prodDate"))
+                        .and(shipService.filterBool(isUsed,"isUsed"))
+                        .and(shipService.filterBetween(minSpeed, maxSpeed,"speed"))
+                        .and(shipService.filterBetween(minCrewSize, maxCrewSize,"crewSize"))
+                        .and(shipService.filterBetween(minRating, maxRating,"rating")),
+                pageable).getContent();
+    }
+
+
+
+    @GetMapping("/rest/ships/count")
+    public long getShipsCount(@RequestParam(value = "name", required = false) String name,
+                              @RequestParam(value = "planet", required = false) String planet,
+                              @RequestParam(value = "shipType", required = false) ShipType shipType,
+                              @RequestParam(value = "after", required = false) Long after,
+                              @RequestParam(value = "before", required = false) Long before,
+                              @RequestParam(value = "isUsed", required = false) Boolean isUsed,
+                              @RequestParam(value = "minSpeed", required = false) Double minSpeed,
+                              @RequestParam(value = "maxSpeed", required = false) Double maxSpeed,
+                              @RequestParam(value = "minCrewSize", required = false) Integer minCrewSize,
+                              @RequestParam(value = "maxCrewSize", required = false) Integer maxCrewSize,
+                              @RequestParam(value = "minRating", required = false) Double minRating,
+                              @RequestParam(value = "maxRating", required = false) Double maxRating) {
+
+        return shipService.getShipsCount(Specification
+                        .where(shipService.filterStringLike(name,"name")
+                        .and(shipService.filterStringLike(planet,"planet")))
+                        .and(shipService.filterEqual(shipType,"shipType"))
+                        .and(shipService.filterBetween(after, before,"prodDate"))
+                        .and(shipService.filterBool(isUsed,"isUsed"))
+                        .and(shipService.filterBetween(minSpeed, maxSpeed,"speed"))
+                        .and(shipService.filterBetween(minCrewSize, maxCrewSize,"crewSize"))
+                        .and(shipService.filterBetween(minRating, maxRating,"rating")));
+    }
+
+
+
+    @PostMapping("/rest/ships/{id}")
+    public Ship updateShipById(@PathVariable Long id, @RequestBody Ship ship) {
+       return shipService.updateShip(id, ship);
+    }
+
+    @PostMapping("/rest/ships")
+    public Ship createShip(@RequestBody Ship ship) {
+        return shipService.createShip(ship);
+    }
+
+    @DeleteMapping("/rest/ships/{id}")
+    public void deleteShipById(@PathVariable Long id)  {
+        shipService.deleteById(id);
+    }
+
+    @GetMapping("/rest/ships/{id}")
+    public Ship getShipById(@PathVariable Long id) {
+        return shipService.getById(id);
+    }
+
+}
---
+++ src/main/java/com/space/service/ShipService.java
@@ -0,0 +1,149 @@
+package com.space.service;
+
+import com.space.controller.ShipParamValidator;
+import com.space.model.Ship;
+import com.space.model.ShipType;
+import com.space.repository.ShipRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.*;
+
+@Service
+@Transactional
+public class ShipService {
+
+    @Autowired
+    ShipRepository repository;
+
+    public Ship createShip (Ship ship) {
+        ShipParamValidator.nullValidate(ship);
+        ShipParamValidator.validate(ship.getName());
+        ShipParamValidator.validate(ship.getPlanet());
+        ShipParamValidator.validate(ship.getSpeed());
+        ShipParamValidator.validate(ship.getCrewSize());
+        ShipParamValidator.validate(ship.getProdDate());
+        if (ship.isUsed() == null) ship.setUsed(false); //if param is not send
+        if (ship.getId() != null) ship.setId(null); //if we receive id put it on null
+        System.out.println(ship);
+        ShipParamValidator.updateRating(ship);
+        return repository.saveAndFlush(ship);
+    }
+
+    public Ship updateShip (Long id,Ship ship) {
+        ShipParamValidator.validate(id);
+        if (!repository.findById(id).isPresent())  ShipParamValidator.Error404("Not Exist ID");
+
+        if (ShipParamValidator.isEmptyBody(ship)) return repository.findById(id).get(); //empty body
+
+        Ship DBship = repository.findById(id).get();
+
+        if (ship.getId() != null) {
+            if (!ship.getId().equals(DBship.getId())) ship.setId(DBship.getId());
+        } else ship.setId(DBship.getId());
+
+        if (ship.getName() != null) {
+            ShipParamValidator.validate(ship.getName());
+        } else ship.setName(DBship.getName());
+
+        if (ship.getPlanet() != null) {
+            ShipParamValidator.validate(ship.getPlanet());
+        } else ship.setPlanet(DBship.getPlanet());
+
+        if (ship.getSpeed() != null) {
+            ShipParamValidator.validate(ship.getSpeed());
+        } else ship.setSpeed(DBship.getSpeed());
+
+        if (ship.getCrewSize() != null) {
+            ShipParamValidator.validate(ship.getCrewSize());
+        } else ship.setCrewSize(DBship.getCrewSize());
+
+        if (ship.getShipType() == null) ship.setShipType(DBship.getShipType());
+
+        if (ship.isUsed() == null) ship.setUsed(DBship.isUsed());
+
+        if (ship.getProdDate() != null) {
+            ShipParamValidator.validate(ship.getProdDate());
+        } else ship.setProdDate(DBship.getProdDate());
+
+        ShipParamValidator.updateRating(ship); //update rating
+
+        return repository.save(ship);
+    }
+
+    public Page<Ship> getAll(Specification<Ship> specification, Pageable pageable) {
+        return repository.findAll(specification,pageable);
+    }
+
+    public long getShipsCount(Specification<Ship> specification) {
+        return repository.count(specification);
+    }
+
+    public Ship getById(Long id) {
+        ShipParamValidator.validate(id);
+
+        if (repository.findById(id).isPresent())
+            return repository.findById(id).get();
+        else
+            ShipParamValidator.Error404("Not Exist ID");
+            return null;
+
+    }
+
+    public void deleteById(Long id){
+        ShipParamValidator.validate(id);
+        if (repository.findById(id).isPresent()) repository.deleteById(id);
+        else ShipParamValidator.Error404("Not Exist ID");
+    }
+
+    /*
+        Filtering methods for search in DB
+     */
+    public Specification<Ship> filterStringLike(String value,String param) {
+        return (root, query, cb) -> value == null ? null : cb.like(root.get(param), "%" + value + "%");
+    }
+
+    public Specification<Ship> filterBetween(Integer min, Integer max, String param ) {
+        return (root, query, cb) -> {
+            if (min == null && max == null) return null;
+            if (min == null) return cb.lessThanOrEqualTo(root.get(param), max);
+            if (max == null) return cb.greaterThanOrEqualTo(root.get(param), min);
+            return cb.between(root.get(param), min, max);
+        };
+    }
+
+    public Specification<Ship> filterBetween(Double min, Double max, String param ) {
+        return (root, query, cb) -> {
+            if (min == null && max == null) return null;
+            if (min == null) return cb.lessThanOrEqualTo(root.get(param), max);
+            if (max == null) return cb.greaterThanOrEqualTo(root.get(param), min);
+            return cb.between(root.get(param), min, max);
+        };
+    }
+
+    public Specification<Ship> filterBetween(Long min, Long max, String param) {
+        return (root, query, cb) -> {
+            if (min == null && max == null) return null;
+            if (min == null) return cb.lessThanOrEqualTo(root.get(param), new Date(max));
+            if (max == null) return cb.greaterThanOrEqualTo(root.get(param), new Date(min));
+            return cb.between(root.get(param), new Date(min), new Date(max));
+        };
+    }
+
+    public Specification<Ship> filterEqual(ShipType shipType, String param) {
+        return (root, query, cb) -> shipType == null ? null : cb.equal(root.get(param), shipType);
+    }
+
+    public Specification<Ship> filterBool(Boolean isUsed, String param) {
+        return (root, query, cb) -> {
+            if (isUsed == null) return null;
+            if (isUsed) return cb.isTrue(root.get(param));
+            return cb.isFalse(root.get(param));
+        };
+    }
+
+
+}
